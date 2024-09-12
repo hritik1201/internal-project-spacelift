3375a725143c9a594191ac76ec434df37cc8b856
hnagpal <hnagpal@qasource.com>
2024-09-12 18:37:04 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+data "aws_vpc" "vpc_existing_id" {
+  id = var.vpc_existing_id
+}
+data "aws_key_pair" "my_key" {
+  key_name          = var.existing_key_name
+  include_public_key = true
+}
+data "aws_security_group" "java_sg" {
+  id   = var.existing_security_group
+  vpc_id = data.aws_vpc.vpc_existing_id.id
+}
+# EC2 Instance
+resource "aws_instance" "ubuntu_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = data.aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [data.aws_security_group.java_sg.id]
+ 
+}
---
+++ vairiable.tf
@@ -0,0 +1,26 @@
+variable "region" {
+    type = string
+}
+variable "secret_id" {
+    type = string
+}
+variable "access_id" {
+  type = string
+  
+}
+variable "ami_id" {
+    type = string
+  
+}
+variable "instance_type" {
+    type = string
+}
+variable "vpc_existing_id" {
+    type = string
+}
+variable "existing_key_name" {
+    type = string
+}
+variable "existing_security_group" {
+    type = string
+}
