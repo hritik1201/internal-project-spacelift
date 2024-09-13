c95ea4e6d6760cfcd0100d0d9ca1825a82924071
hnagpal <hnagpal@qasource.com>
2024-09-13 11:11:38 +0530

    file updated

--- vairiable.tf
+++ vairiable.tf
@@ -1,26 +1,38 @@
 variable "region" {
-    type = string
+  type    = string
+  default = "ap-south-1"
 }
 variable "secret_id" {
-    type = string
+  type    = string
+  default = "5dUQxQkT2djrzNnzbtR5kEgw0m7hdaMRW+i+jh2F"
+
 }
 variable "access_id" {
-  type = string
-  
+  type    = string
+  default = "AKIATNVEVIKTQFWSAVCB"
+
 }
 variable "ami_id" {
-    type = string
-  
+  type    = string
+  default = "ami-0522ab6e1ddcc7055"
+
 }
 variable "instance_type" {
-    type = string
+  type    = string
+  default = "t2.micro"
 }
 variable "vpc_existing_id" {
-    type = string
+  type    = string
+  default = "vpc-0e5232ada44ab81e6"
+
 }
 variable "existing_key_name" {
-    type = string
+  type    = string
+  default = "my-key-pair"
+
 }
 variable "existing_security_group" {
-    type = string
+  type    = string
+  default = "sg-0c84402213547a164"
+
 }
