32fb619e464f3460d48882b63604478ce2b8284e
hritik1201 <61574214+hritik1201@users.noreply.github.com>
2024-09-15 14:43:16 +0530

    Update vairiable.tf

--- vairiable.tf
+++ vairiable.tf
@@ -1,39 +1,24 @@
-variable "region" {
-  type    = string
-  default = "ap-south-1"
+variable "instance_name" {
+  type = string
+  default = "terraform-demo"
 }
-variable "secret_id" {
-  type    = string
-  default = "5dUQxQkT2djrzNnzbtR5kEgw0m7hdaMRW+i+jh2F"
 
-}
-variable "access_id" {
-  type    = string
-  default = "AKIATNVEVIKTQFWSAVCB"
-
-}
 variable "ami_id" {
-  type    = string
+  type = string
   default = "ami-0522ab6e1ddcc7055"
-
 }
+
 variable "instance_type" {
-  type    = string
+  type = string
   default = "t2.micro"
 }
-variable "vpc_existing_id" {
-  type    = string
-  default = "vpc-0e5232ada44ab81e6"
 
-}
-variable "existing_key_name" {
-  type    = string
+variable "aws_keypair" {
+  type = string
   default = "my-key-pair"
-
-}
-variable "existing_security_group" {
-  type    = string
-  default = "sg-0c84402213547a164"
 }
 
-#pusht
+variable "region" {
+  type = string
+  default = "ap-south-1"
+}
