83ea968948609817908b25646ef24c7c33b6ced5
hritik1201 <61574214+hritik1201@users.noreply.github.com>
2024-09-15 14:44:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
-data "aws_vpc" "vpc_existing_id" {
-  id = var.vpc_existing_id
-}
-data "aws_key_pair" "my_key" {
-  key_name          = var.existing_key_name
-  include_public_key = true
-}
-data "aws_security_group" "java_sg" {
-  id   = var.existing_security_group
-  vpc_id = data.aws_vpc.vpc_existing_id.id
-}
-# EC2 Instance
-resource "aws_instance" "ubuntu_instance" {
-  ami           = var.ami_id
+resource "aws_instance" "ec2_instance" {
+  ami = var.ami_id
   instance_type = var.instance_type
-  key_name      = data.aws_key_pair.my_key.key_name
-  vpc_security_group_ids = [data.aws_security_group.java_sg.id]
- 
+  key_name = var.aws_keypair
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
 }
-
