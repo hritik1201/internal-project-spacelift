009228fc3dae0719e7e25fd7ab389ee866fc0c70
hnagpal <hnagpal@qasource.com>
2024-09-15 14:14:27 +0530

    add file

---
+++ security.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.vpc_existing_id.id
+
+   
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
